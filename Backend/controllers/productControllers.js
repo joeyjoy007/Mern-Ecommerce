8c7ccb085bc01d47ec9f0f047a5569283c4894e7
jay <garvitjian5558@gmail.com>
2021-10-16 13:32:43 +0530

    Get Product

--- Backend/controllers/productControllers.js
+++ Backend/controllers/productControllers.js
@@ -2,13 +2,16 @@ const Product = require('../modals/productModal')
 
 
 exports.addProduct = async (req,res)=>{
-    console.log(req.body);
+
 const addProduct = await Product.create(req.body);
 
     res.status(201).json({success:true,addProduct})
 }
 
 
-exports.getAllProducts = (req,res)=>{
-    res.status(200).json({message:"Route is Working"})
+exports.getAllProducts = async (req,res)=>{
+    const products = await Product.find()
+
+    res.status(200).json({message:"Route is Working",products})
+    
 }
