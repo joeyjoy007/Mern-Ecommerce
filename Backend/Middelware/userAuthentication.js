0636cdbc2294d840ac6868b95288141fad526615
jay <garvitjian5558@gmail.com>
2021-10-18 21:40:59 +0530

    adde jwt verify in login

--- Backend/Middelware/userAuthentication.js
+++ Backend/Middelware/userAuthentication.js
@@ -1,10 +1,25 @@
-const catchError = require('../Middelware/asyncMiddelware')
+const catchError = require('../Middelware/asyncMiddelware');
+const ErrorHandler = require('../Utils/errorHandler/errorHandle');
+const User = require('../modals/UserModel')
+const jwt = require('jsonwebtoken')
 
 const authToken =  catchError(async(req,res,next)=>{
- 
+ console.log(1);
     const token = req.cookies.jwtToken;
   
+        if(!token){
+          
+            next(new ErrorHandler("User not allowed",401))
+        }
 
-    console.log(token);
+        const decode = jwt.verify(token,process.env.SECRET_CODE)
+     
+
+        const user = await User.findById(decode.id)
+   
+  
+        req.user = user;
+        next()
+
 })
 module.exports = authToken
