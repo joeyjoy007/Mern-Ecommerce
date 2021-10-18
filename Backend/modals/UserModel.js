515970b71432eb06f28a579418bbb19fbdaab70a
jay <garvitjian5558@gmail.com>
2021-10-18 12:19:14 +0530

    added bcrypt password

--- Backend/modals/UserModel.js
+++ Backend/modals/UserModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const bcrypt = require('bcrypt')
 
 const userSchema = new mongoose.Schema({
 
@@ -40,6 +41,15 @@ const userSchema = new mongoose.Schema({
         recentPasswordToken:String,
     recentPasswordExpire:Date
     
+})
+
+userSchema.pre("save",async function(next){
+if(!this.isModified("password")){
+    next()
+
+}
+
+    this.password = await bcrypt.hash(this.password,10)
 })
 
 const UserModel = mongoose.model("UserModel",userSchema)
