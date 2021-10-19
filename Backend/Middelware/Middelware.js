498ee4c7b750da0caaf63f8fb9a805a80029c941
jay <garvitjian5558@gmail.com>
2021-10-19 22:17:06 +0530

    update error section

--- Backend/Middelware/Middelware.js
+++ Backend/Middelware/Middelware.js
@@ -5,6 +5,16 @@ const ErrorHandler = require('../Utils/errorHandler/errorHandle')
     err.statusCode = err.statusCode || 500;
     err.message = err.message || "Internal server error"
 
+    if(err.code === 11000){
+        message:"Use other email"
+    }
+    if(err.name === "JsonWebTokenError"){
+        message:"Error in Token"
+    }
+    if(err.name === "TokenExpiredError"){
+        message:"token is expired"
+    }
+
     res.status(err.statusCode).json({
         message:"Failed to catch ",
         success:false,
